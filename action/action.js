97f2fe2367a45d60b55c13cd3c1e8ac2a5e8a797
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:02:41 -0600

    Added action GET request

--- action/action.js
+++ action/action.js
@@ -3,4 +3,32 @@ const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db.get()
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get actions." });
+
+      // Error
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+      console.log(err.config);
+    });
+});
+
 module.exports = router;
