30764e8c89eb2380494ae4ff4c66b9eee8a92033
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:18:17 -0600

    Added action POST request

--- action/action.js
+++ action/action.js
@@ -65,4 +65,45 @@ router.get("/:action_id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const { project_id, description, notes, completed } = req.body;
+  const newAction = { project_id, description, notes, completed };
+
+  db.insert(newAction)
+    .then(actionId => {
+      const { id } = actionId;
+
+      db.get(id).then(action => {
+        if (action) {
+          res.status(201).json(action);
+        } else {
+          res.status(400).json({
+            message: "Missing required information to create a new action."
+          });
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to create action." });
+
+      // Error
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+      console.log(err.config);
+    });
+});
+
 module.exports = router;
