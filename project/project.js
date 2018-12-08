d73db2c56419333ef891085bfc182e1e5f65a8cc
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:44:48 -0600

    Added project DELETE request

--- project/project.js
+++ project/project.js
@@ -146,4 +146,42 @@ router.post("/", (req, res) => {
     });
 });
 
+router.delete("/:project_id", (req, res) => {
+  const { project_id } = req.params;
+
+  db.remove(project_id)
+    .then(deleted => {
+      if (deleted) {
+        res.json(deleted);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      res
+        .status(409)
+        .json({ message: `Problem deleting project ${project_id}.` });
+
+      // Error
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+      console.log(err.config);
+    });
+});
+
 module.exports = router;
