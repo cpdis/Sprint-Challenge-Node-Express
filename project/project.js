bdb414c5bcf90c2b6898f19f9341b28e260e9981
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:41:14 -0600

    Added project POST request

--- project/project.js
+++ project/project.js
@@ -105,4 +105,45 @@ router.get("/:project_id/actions", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const { name, description, completed } = req.body;
+  const newProject = { name, description, completed };
+
+  db.insert(newProject)
+    .then(projectId => {
+      const { id } = projectId;
+
+      db.get(id).then(project => {
+        if (project) {
+          res.status(201).json(project);
+        } else {
+          res
+            .status(400)
+            .json({ message: "Missing required information for the project." });
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to create project." });
+
+      // Error
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+      console.log(err.config);
+    });
+});
+
 module.exports = router;
