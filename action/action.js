e1fdcbba2df73a1d3ba379557d3e0e88e8d837af
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:04:33 -0600

    Added action GET request for individual actions

--- action/action.js
+++ action/action.js
@@ -31,4 +31,38 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:action_id", (req, res) => {
+  const { action_id } = req.params;
+
+  db.get(action_id)
+    .then(action => {
+      if (action) {
+        res.json(action);
+      } else {
+        res.status(404).json({ message: "Failed to get actions." });
+      }
+    })
+    .catch(err => {
+      res.status(404).json({ message: "Actions do not exist for this ID" });
+
+      // Error
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+      console.log(err.config);
+    });
+});
+
 module.exports = router;
