0a974d34f866e0ec81c6d7c6137a6bb91a528fdb
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 11:29:09 -0600

    Added basic server and routes to endpoint files

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const configureMiddleware = require("./config/middleware");
+const projectRoute = require("./project/project");
+const actionRoute = require("./action/action");
 
 // Server
 const server = express();
 const PORT = 3000;
 
 // Middleware
+configureMiddleware(server);
+
+server.use("/api/project", projectRoute);
+server.use("/api/action", actionRoute);
 
 server.get("/", (req, res) => {
   res.send("Node...");
