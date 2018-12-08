8bcfcf781aa09f64339efe872178715d5c04c49a
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-08 12:28:12 -0600

    Added action PUT request

--- action/action.js
+++ action/action.js
@@ -144,4 +144,48 @@ router.delete("/:action_id", (req, res) => {
     });
 });
 
+router.put("/:action_id", (req, res) => {
+  const { action_id } = req.params;
+  const { project_id, description, notes, completed } = req.body;
+  const updateAction = { project_id, description, notes, completed };
+
+  if (updateAction) {
+    db.update(action_id, updateAction)
+      .then(action => {
+        if (action) {
+          res.json({ message: "Successfully updated!" });
+        } else {
+          res.status(404).json({
+            message: "The action with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "The action could not be modified." });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide the required information to update the action."
+    });
+
+    // Error
+    if (err.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(err.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", err.message);
+    }
+    console.log(err.config);
+  }
+});
+
 module.exports = router;
